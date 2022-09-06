d05762d3c8be30cee49ef35f20a7bae18233cede
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-09-05 23:10:57 -0700

    pointerdown and pointer move kind of work, but we are getting a not cleared canvas for some reason.

--- scrambled-artwork-puzzle/src/components/Puzzle.js
+++ scrambled-artwork-puzzle/src/components/Puzzle.js
@@ -43,6 +43,7 @@ const Puzzle = ({ data }) => {
   let ref = React.useRef();
   // this variable will house all data about our puzzle (tiles: images, solution, current positions, widths, heights, etc.)
   let solutionTiles = null;
+  let selectedTile = null;
 
   // calculates the current position of the pointer on the canvas
   const pointerLocation = (e) => {
@@ -67,7 +68,7 @@ const Puzzle = ({ data }) => {
     // calculate solutionX and solutionY as we loop through processedTiles (and update processedTiles)
     solutionTiles = calculateSolutionPositions(data.tiles);
 
-    // render solution tiles
+    // render solution tiles TODO render shuffled tiles, write shuffle function!
     for (let solutionTile of solutionTiles) {
       context.drawImage(
         solutionTile.image,
@@ -77,7 +78,93 @@ const Puzzle = ({ data }) => {
       context.strokeRect(solutionTile.xSolution, solutionTile.ySolution, solutionTile.width, solutionTile.height);
     }
 
-    // add pointer down 
+    // add pointer down event handler
+    canvas.onpointerdown = (e) => {
+        const coordinates = pointerLocation(e);
+        console.log(coordinates)
+        for (let tile of solutionTiles) {
+            // console.log(`${tile.x} ${tile.y}`)
+            if (
+                coordinates.x < tile.xSolution ||
+                coordinates.x > tile.xSolution + tile.width ||
+                coordinates.y < tile.ySolution ||
+                coordinates.y > tile.ySolution + tile.height
+            ) {
+                // do nothing, this tile is not located under pointer
+                console.log("you didn't get me!")
+            } else { 
+                console.log("you got me!");
+                selectedTile = tile;
+            }
+        }
+    }
+
+    // onpointermove event handler
+    canvas.onpointermove = (e) => {
+         context.clearRect(0, 0, solutionTiles.width, solutionTiles.height);
+      if (selectedTile !== null) {
+        const coordinates = pointerLocation(e);
+        // clear canvas
+        context.clearRect(0, 0, solutionTiles.width, solutionTiles.height);
+        // render each tile except the selected on
+        for (let tile of solutionTiles) {
+          if (tile !== selectedTile) {
+            context.drawImage(tile.image, tile.xSolution, tile.ySolution);
+            context.strokeRect(
+              tile.xSolution,
+              tile.ySolution,
+              tile.width,
+              tile.height
+            );
+          }
+        }
+        // render the moving tile
+        context.drawImage(
+          selectedTile.image,
+          coordinates.x - selectedTile.width / 2,
+          coordinates.y - selectedTile.height / 2
+        );
+        // identify the current 'drop' position
+        let dropPosition = null;
+        for (let tile of solutionTiles) {
+          if (
+            coordinates.x < tile.xSolution ||
+            coordinates.x > tile.xSolution + tile.width ||
+            coordinates.y < tile.ySolution ||
+            coordinates.y > tile.ySolution + tile.height
+          ) {
+            // do nothing, this tile is not located under pointer
+            //   console.log("you didn't get me!");
+          } else {
+            //   console.log("you got me!");
+            dropPosition = tile;
+          }
+        }
+
+        // draw drop indicator
+        context.save();
+        context.globalAlpha = 0.4;
+        if (
+          dropPosition.height === selectedTile.height &&
+          dropPosition.width === selectedTile.width
+        ) {
+          context.fillStyle = "#00ff00"; // valid move: apply green indicator tile
+        } else {
+          context.fillStyle = "#ff0000"; // invalid move: apply red indicator tile
+        } 
+        context.fillRect(
+          dropPosition.xSolution,
+          dropPosition.ySolution,
+          dropPosition.width,
+          dropPosition.height
+        );
+        context.restore();
+      }
+    }
+
+    canvas.onpointerup = (e) => {
+        selectedTile = null;
+    };
   });
 
   return (
