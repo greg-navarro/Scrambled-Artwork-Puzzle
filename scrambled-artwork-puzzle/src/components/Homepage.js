fbedaeca5f25c3eb5aac17dbefd42816888876dc
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-09-06 14:48:34 -0700

    fetch one page of records from the collections

--- scrambled-artwork-puzzle/src/components/Homepage.js
+++ scrambled-artwork-puzzle/src/components/Homepage.js
@@ -4,7 +4,6 @@ import data from "../test-data/test-data.json";
 // Utility function get images for test data
 const fetchImages = async function (testData) {
   let images = [];
-
   // use batch of Promises to load images and add an image key/value pair to the 'tiles' array
   // picked up this particular technique from Kimbatt @ https://stackoverflow.com/questions/37854355/wait-for-image-loading-to-complete-in-javascript
   let imageRequestPromises = [];
@@ -19,10 +18,19 @@ const fetchImages = async function (testData) {
     })); 
   } // end for-loop
   await Promise.all(imageRequestPromises);
-
   // update tiles array with images, set this value in the data object, and return that
   for (let i = 0; i < testData.tiles.length; i++) { testData.tiles[i].image = images[i]; }
   return testData;
+}
+
+// Fetch records to fill 'finder' when no search has been performed
+const fetchRecords = async function () {
+  let objectRecords = [];
+  await fetch("https://www.rijksmuseum.nl/api/nl/collection?imgonly=true&key=geRR5dZh")
+  .then(response => response.json())
+  .then(response => objectRecords = response.artObjects);
+  console.log(objectRecords);
+  return objectRecords;
 }
 
 // React component
@@ -33,6 +41,8 @@ const Homepage = ({ startPuzzle, setPuzzleData }) => {
     });
     let dataWithImages = null;
     fetchImages(currentTestLevel).then(value => dataWithImages = value);
+
+    fetchRecords()
 
     return (
       <div>
