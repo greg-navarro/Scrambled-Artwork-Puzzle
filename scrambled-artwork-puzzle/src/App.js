95345b1757b94b78248f0e6a2a2282e82ba28a49
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-09-05 17:51:40 -0700

    set up states and successfully switch states from homepage to puzzle

--- scrambled-artwork-puzzle/src/App.js
+++ scrambled-artwork-puzzle/src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
 import Homepage from './components/Homepage';
 import Puzzle from './components/Puzzle';
 import './App.css';
 
 function App() {
+  // State constants to control conditional component rendering
+  const INTIALSTATE = 1;
+  const PUZZLESTATE = 2;
+  const SUCCESSSTATE = 3;
+
+  const [currentState, setCurrentState] = React.useState(INTIALSTATE);
+
+  const setPuzzleState = () => { setCurrentState(2); };
+
+
   return (
     <div className="App">
-      Empty App!
+      {currentState === INTIALSTATE && <Homepage startPuzzle={setPuzzleState} />}
+      {currentState === PUZZLESTATE && <Puzzle />}
     </div>
   );
 }
--- scrambled-artwork-puzzle/src/components/Homepage.js
+++ scrambled-artwork-puzzle/src/components/Homepage.js
@@ -3,10 +3,15 @@ import data from "../test-data/test-data.json";
 const levels = data.levels;
 const currentTestLevel = levels.find(level => { return level.name === "z4"});
 
-const Homepage = () => {
+const Homepage = ({ startPuzzle }) => {
 
 
-    return <div>Homepage</div>;
+    return (
+      <div>
+        <div>Homepage</div>
+        <button onClick={() => startPuzzle()}>Start puzzle</button>
+      </div>
+    );
 }
 
 export default Homepage;
