9b71a226ca941989fec7e9979f48fade6f5fa78e
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-09-06 15:27:37 -0700

    sourcing data from API

--- scrambled-artwork-puzzle/src/components/Homepage.js
+++ scrambled-artwork-puzzle/src/components/Homepage.js
@@ -29,25 +29,65 @@ const fetchRecords = async function () {
   await fetch("https://www.rijksmuseum.nl/api/nl/collection?imgonly=true&key=geRR5dZh")
   .then(response => response.json())
   .then(response => objectRecords = response.artObjects);
-  console.log(objectRecords);
+  // console.log(objectRecords);
   return objectRecords;
 }
 
+const fetchObjectData = async function (objectNumber) {
+  let data = null;
+  const url = `https://www.rijksmuseum.nl/api/nl/collection/${objectNumber}/tiles?key=geRR5dZh`;
+  data = await fetch(url).then(response => response.json());
+  return data;
+}
+
+
+
 // React component
 const Homepage = ({ startPuzzle, setPuzzleData }) => {
-    const levels = data.levels;
-    const currentTestLevel = levels.find((level) => {
-      return level.name === "z1";
-    });
-    let dataWithImages = null;
-    fetchImages(currentTestLevel).then(value => dataWithImages = value);
+    // const levels = data.levels;
+    // const currentTestLevel = levels.find((level) => {
+    //   return level.name === "z1";
+    // });
+    // let dataWithImages = null;
+    // fetchImages(currentTestLevel).then(value => dataWithImages = value);
 
-    fetchRecords()
+    // Starting new functionality here
+    fetchRecords() // initial call to fetch records
+    let objectNumber = 'SK-C-5'; //null;
+
+    const fetchDataStartPuzzle = async function (objectNumber) {
+      // fetch tiles
+      let objectData = await fetchObjectData(objectNumber);
+      let levelData = objectData.levels.find((level) => {
+        return level.name === "z3";
+      });
+      
+      // setPuzzleData with the tiles
+      const levelDataWithImages = await fetchImages(levelData);
+      console.log(levelDataWithImages);
+      setPuzzleData(levelDataWithImages);
+      // once this is over, call startPuzzle()
+      
+      startPuzzle();
+    };
+
+    const startPuzzleClickHandler = () => {
+      if (objectNumber === null) {
+        alert("pick a result, then press the button")
+      } else {
+        fetchDataStartPuzzle(objectNumber)
+      }
+    }
+
+    const result = (artObject) => {
+      return <li key={artObject.objectNumber} onClick={() => { objectNumber = artObject.objectNumber }}></li>
+    }
 
     return (
       <div>
         <div>Homepage</div>
-        {<button onClick={() => { setPuzzleData(dataWithImages); startPuzzle(); }}>Start puzzle</button>}
+        <ul className="results-container"></ul>
+        {<button onClick={() => { startPuzzleClickHandler() }}>Start puzzle</button>}
       </div>
     );
 }
